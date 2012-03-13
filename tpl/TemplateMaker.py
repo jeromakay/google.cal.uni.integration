61c7f5f016c0a81e0384dafe53ffc25073f464e1
jacxel <tomwjack@hotmail.co.uk>
2012-03-13 19:19:06 +0000

    Adding TemplateMaker.py and associated files
    added new body templates for adding a module and updating a group.

---
+++ tpl/TemplateMaker.py
@@ -0,0 +1,11 @@
+import os
+from google.appengine.ext.webapp import template
+
+def make(self, pageTitle, bodyTemplate ):
+	template_values = {
+		'page_title': pageTitle,
+	}
+	headerPath = os.path.join(os.path.dirname(__file__), 'header.html')
+	bodyPath = os.path.join(os.path.dirname(__file__), bodyTemplate+'.html')
+	footerPath = os.path.join(os.path.dirname(__file__), 'footer.html')
+	self.response.out.write(template.render(headerPath, template_values) + template.render(bodyPath, template_values) + template.render(footerPath, template_values))
