9cbc57d5e6a0aa1dad6e5249d86822c178fd1532
Brian Walsh <cheesyporridge@gmail.com>
2012-03-20 17:58:21 +0000

    All tables have 1 default value. Pretend the tables start at 1.

--- Database/__init__.py
+++ Database/__init__.py
@@ -3,4 +3,6 @@ import DB
 if __name__ == '__init__':
     DB.CreateGroupType('default','A blank test group type')
     DB.CreateGroup(1, 'lol', 'pies', 0)
+    DB.CreateModule(1, 'rofl','çopter')
+    DB.CreateUser(1, 1, 'Herp Derpington')
     pass
