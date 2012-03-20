83dd2962cb069bc8dfb2c107473071f84eec29a5
jacxel <tomwjack@hotmail.co.uk>
2012-03-20 19:43:58 +0000

    deleting stuff thats not needed

---
+++ uccal/fetchers.py
@@ -0,0 +1,15 @@
+return """// groups
+				json = {results: [ 
+								{id: 1, group_type: 1, group_gid: "2dabd109cf892b94e72563da710669c4", title: "IT WORKED, CLAM DOWN", description: "Everyone in Web Systems Engineering" }, 
+								{id: 2, group_type: 1, group_gid: "ee8efdd53326e275390b866473aac247", title: "food_and_science_ck401_3rd_cs", description: "Everyone in Computer Science Hons" }, 
+								{id: 3, group_type: 1, group_gid: "9f1319fde74c8ca728ec7452d4e85521", title: "arts_music", description: "Everyone studying Music Hons." }, 
+								{id: 4, group_type: 2, group_gid: "9459e2b377f25454ee47133c03205076", title: "arts_music_trad_music_lecturers", description: "Lecturers teaching Trad Irish Music." }, 
+								{id: 5, group_type: 3, group_gid: "7e9cf516ec7fcc55476fe44696cfca66", title: "foot_and_science_microbiology_lab_assist", description: "Labs assistants for the microbiology lecture's labs." },
+								{id: 6, group_type: 1, group_gid: "f94d66f27497e84e10b2843ee718a078", title: "erasmus_2011_Juan_Carlos_Montero_1 09868111", description: "Erasmus 2011 student, Juan Carlos Montero." },
+								{id: 7, group_type: 1, group_gid: "wse@jeromakay.com", title: "real_deal", description: "This is the real deal." } ],
+								
+							groupTypes: [
+								{ id: 1, name: "students" },
+								{ id: 2, name: "lecturers" },
+								{ id: 3, name: "assistants" }
+							] }; """
