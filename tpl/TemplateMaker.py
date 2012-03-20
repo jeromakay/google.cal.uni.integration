45deb0f9eac6e0378931241cfd69cd934d1abdc7
jacxel <tomwjack@hotmail.co.uk>
2012-03-20 19:34:49 +0000

    commiting templates.

--- tpl/TemplateMaker.py
+++ tpl/TemplateMaker.py
@@ -5,7 +5,7 @@ def make(self, pageTitle, bodyTemplate ):
 	template_values = {
 		'page_title': pageTitle,
 	}
-	headerPath = os.path.join(os.path.dirname(__file__), 'header.html')
+	headerPath = os.path.join(os.path.dirname(__file__), 'root/header.html')
 	bodyPath = os.path.join(os.path.dirname(__file__), bodyTemplate+'.html')
-	footerPath = os.path.join(os.path.dirname(__file__), 'footer.html')
+	footerPath = os.path.join(os.path.dirname(__file__), 'root/footer.html')
 	self.response.out.write(template.render(headerPath, template_values) + template.render(bodyPath, template_values) + template.render(footerPath, template_values))
